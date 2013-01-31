f78243b47bb10d376d33938d2195d9cd37a53870
Raj Parameswaran <rajpara@google.com>
2013-01-31 14:32:50 -0800

    Making update so example works with AdX as well.

--- examples/BannerExample/Classes/SampleConstants.h
+++ examples/BannerExample/Classes/SampleConstants.h
@@ -8,4 +8,5 @@
 
 // You should define this for your account before compiling.
 // For AdMob, this is your site ID.
-#define kSampleAdUnitID @"INSERT_YOUR_ADMOB_ID_HERE"
+// For Ad Exchange, this is <google_ad_client>/<google_ad_slot>
+#define kSampleAdUnitID @"INSERT_YOUR_AD_UNIT_ID_HERE"
