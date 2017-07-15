9f024e5bcbbb266c73bd3b95dbb7ae17b65d2a24
Yoshiyuki Kawashima <ykawashi7@gmail.com>
2017-07-15 10:40:53 +0900

    Replace "FIRApp" with "FirebaseApp"
    'FIRApp' has been renamed to 'FirebaseApp'
    
    fixes #55

--- Swift/admob/NativeAdvancedExample/NativeAdvancedExample/AppDelegate.swift
+++ Swift/admob/NativeAdvancedExample/NativeAdvancedExample/AppDelegate.swift
@@ -25,7 +25,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication,
       didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
     // Use Firebase library to configure APIs.
-    FIRApp.configure()
+    FirebaseApp.configure()
     // Initialize Google Mobile Ads SDK.
     GADMobileAds.configure(withApplicationID: "ca-app-pub-3940256099942544~1458002511")
 
