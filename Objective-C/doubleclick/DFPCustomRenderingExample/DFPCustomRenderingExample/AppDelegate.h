52e0cb4b568913a0c19bc02fc8f1d7e573c50802
Samuel Stow <samuelstow@google.com>
2018-08-16 17:19:29 -0400

    Updated native implementation to correctly display media view without borders

--- Objective-C/doubleclick/DFPCustomRenderingExample/DFPCustomRenderingExample/AppDelegate.h
+++
@@ -1,9 +0,0 @@
-//  Copyright (c) 2015 Google. All rights reserved.
-
-@import UIKit;
-
-@interface AppDelegate : UIResponder<UIApplicationDelegate>
-
-@property(nonatomic, strong) UIWindow *window;
-
-@end
