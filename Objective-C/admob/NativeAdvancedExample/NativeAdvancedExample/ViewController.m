ee5c2135b88186dee56e778f15b825417a76130c
Samuel Stow <samuelstow@google.com>
2018-08-16 17:19:29 -0400

    Updated native implementation to correctly display media view without borders

--- Objective-C/admob/NativeAdvancedExample/NativeAdvancedExample/ViewController.m
+++ Objective-C/admob/NativeAdvancedExample/NativeAdvancedExample/ViewController.m
@@ -130,15 +130,7 @@ static NSString *const TestAdUnit = @"ca-app-pub-3940256099942544/3986624511";
   // Some native ads will include a video asset, while others do not. Apps can
   // use the GADVideoController's hasVideoContent property to determine if one
   // is present, and adjust their UI accordingly.
-
-  // The UI for this controller constrains the image view's height to match the
-  // media view's height, so by changing the one here, the height of both views
-  // are being adjusted.
   if (nativeAd.videoController.hasVideoContent) {
-    // The video controller has content. Show the media view.
-    nativeAdView.mediaView.hidden = NO;
-    nativeAdView.imageView.hidden = YES;
-
     // This app uses a fixed width for the GADMediaView and changes its height
     // to match the aspect ratio of the video it displays.
     if (nativeAd.videoController.aspectRatio > 0) {
@@ -159,14 +151,6 @@ static NSString *const TestAdUnit = @"ca-app-pub-3940256099942544/3986624511";
 
     self.videoStatusLabel.text = @"Ad contains a video asset.";
   } else {
-    // If the ad doesn't contain a video asset, the first image asset is shown
-    // in the image view. The existing lower priority height constraint is used.
-    nativeAdView.mediaView.hidden = YES;
-    nativeAdView.imageView.hidden = NO;
-
-    GADNativeAdImage *firstImage = nativeAd.images.firstObject;
-    ((UIImageView *)nativeAdView.imageView).image = firstImage.image;
-
     self.videoStatusLabel.text = @"Ad does not contain a video.";
   }
 
