3a71efc1f31afeb42592a2e1bff28133f5cc4a4b
Samuel Stow <samuelstow@google.com>
2019-11-25 14:03:02 -0800

    Fix missing imports in APIDemo swift sample.
    PiperOrigin-RevId: 282428722

--- Swift/advanced/APIDemo/APIDemo/AdManagerPPIDViewController.swift
+++ Swift/advanced/APIDemo/APIDemo/AdManagerPPIDViewController.swift
@@ -12,6 +12,10 @@
 //  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
+//
+
+import GoogleMobileAds
+import UIKit
 
 class AdManagerPPIDViewController: UIViewController {
 
