f16961b773cc684a9c976e61b733b79772bc4101
Mobile Ads Developer Relations <mad-devrel@google.com>
2021-10-28 01:49:58 -0700

    iOS Swift App Open Ad sample - Simulate the app is going to take 5 seconds to be initialize.
    PiperOrigin-RevId: 406084059

--- Swift/admob/AppOpenExample/AppOpenExample/AppDelegate.swift
+++ Swift/admob/AppOpenExample/AppOpenExample/AppDelegate.swift
@@ -21,110 +21,29 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate, GADFullScreenContentDelegate {
 
   var window: UIWindow?
-  var appOpenAd: GADAppOpenAd?
-  var isLoadingAd = false
-  var isShowingAd = false
-  /// Keeps track of the time an app open ad is loaded to ensure you don't show an expired ad.
-  var loadTime = Date()
 
   func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-
-    // Initialize Google Mobile Ads SDK
+    // Initialize Google Mobile Ads SDK.
     GADMobileAds.sharedInstance().start(completionHandler: nil)
+    // Although the Google Mobile Ads SDK might not be fully initialized at this point,
+    // we should still load the app open ad so it becomes ready to show when the splash
+    // screen dismisses.
+    AppOpenAdManager.shared.loadAd()
     return true
   }
 
   func applicationDidBecomeActive(_ application: UIApplication) {
-    if let currentVC = application.keyWindow?.rootViewController {
-      // Show the ad (if available) when the app moves from the inactive to active state.
-      showAdIfAvailable(viewController: currentVC)
-    }
-  }
-
-  func loadAd() {
-    // Do not load ad if there is an unused ad or one is already loading.
-    if isLoadingAd || isAdAvailable() {
-      return
-    }
-    isLoadingAd = true
-    print("Start loading ad.")
-    GADAppOpenAd.load(
-      withAdUnitID: "ca-app-pub-3940256099942544/5662855259",
-      request: GADRequest(),
-      orientation: UIInterfaceOrientation.portrait
-    ) { ad, error in
-      if let error = error {
-        self.isLoadingAd = false
-        print("App open ad failed to load with error: \(error.localizedDescription).")
+    let rootViewController = application.windows.first(
+      where: { $0.isKeyWindow })?.rootViewController
+    if let rootViewController = rootViewController {
+      // Do not show app open ad if the current view controller is SplashViewController.
+      if rootViewController is SplashViewController {
         return
       }
-
-      self.appOpenAd = ad
-      self.appOpenAd?.fullScreenContentDelegate = self
-      self.isLoadingAd = false
-      self.loadTime = Date()
-      print("Loading Succeeded.")
+      AppOpenAdManager.shared.showAdIfAvailable(viewController: rootViewController)
     }
   }
-
-  func wasLoadTimeLessThanNHoursAgo(numHours: Int) -> Bool {
-    // Check if ad was loaded more than n hours ago.
-    let timeIntervalBetweenNowAndLoadTime = Date().timeIntervalSince(loadTime)
-    let secondsPerHour = 3600.0
-    let intervalInHours = timeIntervalBetweenNowAndLoadTime / secondsPerHour
-    return intervalInHours < Double(numHours)
-  }
-
-  func isAdAvailable() -> Bool {
-    // Check if ad exists and can be shown.
-    // Ad references in the app open beta will time out after four hours, but this time limit
-    // may change in future beta versions. For details, see:
-    // https://support.google.com/admob/answer/9341964?hl=en
-    return appOpenAd != nil && wasLoadTimeLessThanNHoursAgo(numHours: 4)
-  }
-
-  func showAdIfAvailable(viewController: UIViewController) {
-    // If the app open ad is already showing, do not show the ad again.
-    if isShowingAd {
-      print("The app open ad is already showing.")
-      return
-    }
-    // If the app open ad is not available yet, invoke the callback then load the ad.
-    if !isAdAvailable() {
-      print("The app open ad is not ready yet.")
-      loadAd()
-      return
-    }
-    if let ad = appOpenAd {
-      print("Will show ad.")
-      isShowingAd = true
-      ad.present(fromRootViewController: viewController)
-    }
-  }
-
-  // MARK: GADFullScreenContentDelegate
-  func adDidPresentFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-    print("App open ad presented.")
-  }
-
-  func adDidDismissFullScreenContent(_ ad: GADFullScreenPresentingAd) {
-    appOpenAd = nil
-    isShowingAd = false
-    print("App open ad dismissed.")
-    loadAd()
-  }
-
-  func ad(
-    _ ad: GADFullScreenPresentingAd,
-    didFailToPresentFullScreenContentWithError error: Error
-  ) {
-    appOpenAd = nil
-    isShowingAd = false
-    print("App open ad failed to present with error: \(error.localizedDescription).")
-    loadAd()
-  }
-
 }
--- Swift/admob/AppOpenExample/AppOpenExample/MainViewController.swift
+++ Swift/admob/AppOpenExample/AppOpenExample/MainViewController.swift
@@ -16,7 +16,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
