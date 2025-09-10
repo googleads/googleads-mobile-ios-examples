6075248c413e53883f277d2f4ec5b5d235491355
Nicholas Ventimiglia <nventimiglia@google.com>
2025-09-10 14:08:57 -0700

    Added snippets for server to server requests.
    PiperOrigin-RevId: 805508507

---
+++ Swift/advanced/APIDemo/APIDemo/Snippets/AdManagerSCARSnippets.swift
@@ -0,0 +1,82 @@
+//  Copyright (C) 2025 Google, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+import GoogleMobileAds
+import UIKit
+
+class AdManagerSCARSnippets: NSObject {
+
+  // [START signal_request_native]
+  func loadNative(adUnitID: String) async {
+    // Create a signal request for an ad.
+    // Specify the "signal_type_ad_manager_s2s" to
+    // denote that the usage of QueryInfo is for Ad Manager S2S.
+    let signalRequest = NativeSignalRequest(signalType: "signal_type_ad_manager_s2s")
+    signalRequest.requestAgent = "REQUEST_AGENT"
+    signalRequest.adUnitID = adUnitID
+
+    do {
+      let signal = try await MobileAds.generateSignal(signalRequest)
+      print("Signal string: \(signal.signal)")
+      // TODO: Fetch the ad response using your generated signal.
+    } catch {
+      print("Error getting ad info: \(error.localizedDescription)")
+    }
+  }
+  // [END signal_request_native]
+
+  // [START signal_request_banner]
+  func loadBanner(adUnitID: String) async {
+    // Create a signal request for an ad.
+    // Specify the "signal_type_ad_manager_s2s" to
+    // denote that the usage of QueryInfo is for Ad Manager S2S.
+    let signalRequest = BannerSignalRequest(signalType: "signal_type_ad_manager_s2s")
+    signalRequest.requestAgent = "REQUEST_AGENT"
+    signalRequest.adUnitID = adUnitID
+    // Refer to the AdSize class for available ad sizes.
+    signalRequest.adSize = currentOrientationInlineAdaptiveBanner(width: 375)
+
+    do {
+      let signal = try await MobileAds.generateSignal(signalRequest)
+      print("Signal string: \(signal.signal)")
+      // TODO: Fetch the ad response using your generated signal.
+    } catch {
+      print("Error getting ad info: \(error.localizedDescription)")
+    }
+  }
+  // [END signal_request_banner]
+
+  // [START signal_request_native_plus_banner]
+  func loadNativePlusBanner(adUnitID: String) async {
+    // Create a signal request for an ad.
+    // Specify the "signal_type_ad_manager_s2s" to
+    // denote that the usage of QueryInfo is for Ad Manager S2S.
+    let signalRequest = NativeSignalRequest(signalType: "signal_type_ad_manager_s2s")
+    signalRequest.requestAgent = "REQUEST_AGENT"
+    signalRequest.adUnitID = adUnitID
+    signalRequest.adLoaderAdTypes = [AdLoaderAdType.native, AdLoaderAdType.adManagerBanner]
+    // Refer to the AdSize class for available ad sizes.
+    signalRequest.adSizes = [nsValue(for: currentOrientationInlineAdaptiveBanner(width: 375))]
+
+    do {
+      let signal = try await MobileAds.generateSignal(signalRequest)
+      print("Signal string: \(signal.signal)")
+      // TODO: Fetch the ad response using your generated signal.
+    } catch {
+      print("Error getting ad info: \(error.localizedDescription)")
+    }
+  }
+  // [END signal_request_native_plus_banner]
+}
